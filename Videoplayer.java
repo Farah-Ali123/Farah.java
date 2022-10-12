dd27f64705b45cc4da33c69cedff168a1bc8c9f7
Farah <farahalinoondani@gmail.com>
2022-10-11 23:39:55 -0700

    This is my project

---
+++ Videoplayer.java
@@ -0,0 +1,32 @@
+//      EXCEPTION HANDLING
+ /*class Videooplayer{
+    String [] arr={"mp4","mvo","wav","avi"};
+    public void check(String s){
+        boolean flag=false;
+        for(int i=0;i<4;i++){
+            if(s==arr[i]){
+                flag=true;
+            }
+        }
+        try{
+            if(flag==true){
+                System.out.println("Matched");
+            }
+            else {
+                System.out.println("unmatched");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+}
+class Videoplayer{
+    public static void main(String[] args) {
+        Videooplayer v=new Videooplayer();
+        v.check("mp4");
+        v.check("abc");
+    }
+}
+*/
